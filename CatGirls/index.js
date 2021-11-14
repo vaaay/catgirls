22515d7d3e65f752cbdc19d90e719162dbd6a2cd
vaaay <93358679+vaaay@users.noreply.github.com>
2021-11-14 00:39:40 -0500

    Add files via upload

--- CatGirls/index.js
+++ CatGirls/index.js
@@ -1,15 +1,34 @@
-// CHANGELOG v1.1.0 = - Fixed bug that swapped cat girls too fast spamming your screen (Thanks Unclaimed <3)
+// CHANGELOG v1.1.0 = - Fixed bug that swapped cat girls too fast spamming your screen (Thanks Unclaimed and Debug <3)
 //                    - Added Cat Boy mode and NSFW mode
+// CHANGELOG v1.2.0 = - Added Custom CatGirls Image setting.
+//                    - Added lots of new Cat girls and Cat boys
 
 import Settings from "./settings"
 import settings from "./settings"
+
+let a1 = settings.image1address.replace('https://i.imgur.com/', '')
+let a2 = settings.image2address.replace('https://i.imgur.com/', '')
+let a3 = settings.image3address.replace('https://i.imgur.com/', '')
+let a4 = settings.image4address.replace('https://i.imgur.com/', '')
+let a5 = settings.image5address.replace('https://i.imgur.com/', '')
 
 const imagesa = [
   new Image("catgirl1.png", "https://i.imgur.com/hGJEqWs.png"),
   new Image("catgirl2.png", "https://i.imgur.com/zEjvcyX.png"),
   new Image("catgirl3.png", "https://i.imgur.com/S9cIM1h.png"),
   new Image("catgirl4.png", "https://i.imgur.com/by4hljL.png"),
-  new Image("catgirl5.png", "https://i.imgur.com/BFSv5kB.png")
+  new Image("catgirl5.png", "https://i.imgur.com/BFSv5kB.png"),
+  new Image("catgirl6.png", "https://i.imgur.com/lo3RwkE.png"),
+  new Image("catgirl7.png", "https://i.imgur.com/lV4SWQm.png"),
+  new Image("catgirl8.png", "https://i.imgur.com/Lw2gC6B.png")
+];
+
+const customImages = [
+  new Image(`${a1}`, `${settings.image1address}`),
+  new Image(`${a2}`, `${settings.image2address}`),
+  new Image(`${a3}`, `${settings.image3address}`),
+  new Image(`${a4}`, `${settings.image4address}`),
+  new Image(`${a5}`, `${settings.image5address}`)
 ];
 
 const nsfwimages = [
@@ -21,11 +40,15 @@ const nsfwimages = [
 const catboyimages = [
   new Image("catboy1.png", "https://i.imgur.com/GhRSZYl.png"),
   new Image("catboy2.png", "https://i.imgur.com/Dsyb66E.png"),
-  new Image("catboy3.png", "https://i.imgur.com/1EebUvv.png")
+  new Image("catboy3.png", "https://i.imgur.com/1EebUvv.png"),
+  new Image("catboy4.png", "https://i.imgur.com/MiATTVr.png"),
+  new Image("catboy5.png", "https://i.imgur.com/qpZFmIe.png"),
+  new Image("catboy6.png", "https://i.imgur.com/JFeyydO.png")
 ]
 
 let img;
-if (settings.nsfwMode) {img = nsfwimages[1]}
+if (settings.customCatGirls) {img = customImages[1]}
+else if (settings.nsfwMode) {img = nsfwimages[1]}
 else if (settings.catboyMode) {img = catboyimages[1]}
 else {
   img = imagesa[1];
@@ -34,7 +57,12 @@ let ranwidth = 100;
 
 
 const getRandomImage = () => {
-    if (settings.nsfwMode) {
+    if (settings.customCatGirls) {
+      var imageToUse = customImages[Math.floor(Math.random() * customImages.length)];
+      return imageToUse;
+    }
+
+    else if (settings.nsfwMode) {
       var imageToUse = nsfwimages[Math.floor(Math.random() * nsfwimages.length)];
       return imageToUse;
     }
--- CatGirls/settings.js
+++ CatGirls/settings.js
@@ -1,5 +1,6 @@
 import {
     @SwitchProperty,
+    @TextProperty,
     @Vigilant,
 } from '../Vigilance/index';
 
@@ -9,6 +10,7 @@ class Settings {
         constructor() {
             this.initialize(this);
             this.setCategoryDescription("General", "&3Catgirls are great!")
+            this.setCategoryDescription("Custom CatGirls", "&3You know what's better than Cat Girls? Cat Girls that you specifically choose to your liking!")
         }
 
 
@@ -29,6 +31,58 @@ class Settings {
         })
         catboyMode = false;
 
+        @SwitchProperty({
+            name: "Use own custom CatGirls",
+            description: "Use your own custom CatGirl images. Run /ct load after setting all the image addresses to activate them.",
+            category: "Custom CatGirls",
+            subcategory: "Customize"
+        })
+        customCatGirls = false;
+
+        @TextProperty({
+            name: "Custom CatGirl image 1 address",
+            description: "This is the image address for your custom catgirl image 1.",
+            category: "Custom CatGirls",
+            subcategory: "Cutomize Images",
+            placeholder: "https://i.imgur.com/hGJEqWs.png",
+        })
+        image1address = "https://i.imgur.com/hGJEqWs.png";
+
+        @TextProperty({
+            name: "Custom CatGirl image 2 address",
+            description: "This is the image address for your custom catgirl image 2.",
+            category: "Custom CatGirls",
+            subcategory: "Cutomize Images",
+            placeholder: "https://i.imgur.com/zEjvcyX.png",
+        })
+        image2address = "https://i.imgur.com/zEjvcyX.png";
+
+        @TextProperty({
+            name: "Custom CatGirl image 3 address",
+            description: "This is the image address for your custom catgirl image 3.",
+            category: "Custom CatGirls",
+            subcategory: "Cutomize Images",
+            placeholder: "https://i.imgur.com/S9cIM1h.png",
+        })
+        image3address = "https://i.imgur.com/S9cIM1h.png";
+
+        @TextProperty({
+            name: "Custom CatGirl image 4 address",
+            description: "This is the image address for your custom catgirl image 4.",
+            category: "Custom CatGirls",
+            subcategory: "Cutomize Images",
+            placeholder: "https://i.imgur.com/by4hljL.png",
+        })
+        image4address = "https://i.imgur.com/by4hljL.png";
+
+        @TextProperty({
+            name: "Custom CatGirl image 5 address",
+            description: "This is the image address for your custom catgirl image 5.",
+            category: "Custom CatGirls",
+            subcategory: "Cutomize Images",
+            placeholder: "https://i.imgur.com/BFSv5kB.png",
+        })
+        image5address = "https://i.imgur.com/BFSv5kB.png";
 
 }
 
